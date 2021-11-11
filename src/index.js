ed6ce8d27c5affb088c4b4fcaf7e9dba902b9cbf
nillkizz <kotovvsan@yandex.ru>
2021-11-12 00:51:07 +0300

    Fixed mobile cursor visible

--- src/index.js
+++ src/index.js
@@ -7,61 +7,78 @@ import { debug, DOMElement } from './helpers.js'
 
 class Cursor {
   constructor() {
-    this.x = 0;
-    this.y = 0;
     this.elId = 'ipad-cursor'
+    this.evListeners = []
     this.size = 20
     this.width = 20
     this.height = 20
-    this.cx = this.width / 2
-    this.cy = this.height / 2
     this.color = '#00000080'
-    this.isVisible = false
+    this.pos = {
+      x: 0,
+      y: 0,
+    }
+    this.cache = {
+      isVisible: false
+    }
+    this.mouseMoveCount = 0
     this.init();
 
     this.debugData = {}
     this.updateDebug()
     this.debugFrames = 0
   }
+  get cx() { return this.pos.x - this.width / 2 }
+  get cy() { return this.pos.y - this.height / 2 }
+  get isVisible() { return this.mouseMoveCount > 1 }
   init() {
     this.DE = new DOMElement('div').setAttrs({ id: this.elId }).appendChildTo('body');
     this.el = this.DE.el;
     this.svg = SVG().addTo(`#${this.el.id}`).size(this.width, this.height).circle(this.size).fill(this.color)
-    document.addEventListener('mousemove', this.toggleVisible(true), { once: true })
-    document.addEventListener('mousemove', this.onMouseMove.bind(this))
-    document.body.addEventListener('mouseenter', () => this.toggleVisible(true))
-    document.body.addEventListener('mouseleave', () => this.toggleVisible(false))
     this.animate();
+    this.initEvListeners();
+  }
+  initEvListeners() {
+    this.evListeners = [
+      document.body.addEventListener('mousemove', (e) => this.onMouseMove(e)),
+      document.body.addEventListener('mouseleave', () => this.hide()),
+      document.body.addEventListener('touchend', () => this.hide())
+    ]
   }
   onMouseMove(e) {
-    this.x = e.clientX;
-    this.y = e.clientY;
+    this.pos.x = e.clientX;
+    this.pos.y = e.clientY;
+    this.mouseMoveCount++
   }
-  toggleVisible(val) {
-    if (val == this.isVisible) return
-    if (typeof val === "boolean") {
-      this.el.classList.toggle('visible', val)
-      this.isVisible = val
-    } else {
-      this.isVisible = !this.visible
-      this.el.classList.toggle('visible', this.isVisible)
-    }
+  hide() {
+    this.mouseMoveCount = 0
   }
   animate() {
-    const element = document.elementFromPoint(this.x, this.y);
+    const newValues = {
+      isVisible: this.isVisible,
+    }
+    // const element = document.elementFromPoint(this.pos.x, this.pos.y);
+    if (this.cache.isVisible != newValues.isVisible) {
+      this.el.classList.toggle('visible', newValues.isVisible)
+    }
 
-    this.el.style.left = this.x - this.cx + 'px'
-    this.el.style.top = this.y - this.cy + 'px'
+    this.el.style.left = this.cx + 'px'
+    this.el.style.top = this.cy + 'px'
+    this.updateCache(newValues)
     requestAnimationFrame(this.animate.bind(this))
   }
 
+  updateCache(newValues) {
+    Object.assign(this.cache, newValues);
+  }
+
   updateDebug() {
-    const skipFrames = 6;
-    const hasUpdates = JSON.stringify(this.debugData) != JSON.stringify(debugData);
-    const isThrottling = this.debugFrames % skipFrames != 0;
     const debugData = {
       visible: this.isVisible,
     }
+
+    const skipFrames = 6;
+    const isThrottling = this.debugFrames % skipFrames != 0;
+    const hasUpdates = JSON.stringify(this.debugData) != JSON.stringify(debugData);
 
     if (hasUpdates && !isThrottling) {
       debug(debugData)
